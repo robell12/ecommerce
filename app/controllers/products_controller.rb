82cae6a55563e0281fc1dcf45f45a9208bdf1084
Robert Bell <robell7837@hotmail.com>
2016-11-11 17:20:27 -0700

    New product controller and stuff

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,54 @@
 class ProductsController < ApplicationController
+ 	before_action :set_categories
+ 	before_action :set_products, except: [:index, :new, :create]
+
   def index
+  	@products = @category.products
   end
 
   def new
+  	@product = @category.products.new
   end
 
   def edit
   end
 
+  def create
+  	@product = @category.products.new(product_params)
+  	if @product.save
+  		redirect_to category_product_path(@category, @product)
+  	else
+  		render :new
+  	end
+  end
+
+  def update
+  	if @product.update(product_params)
+  		redirect_to category_product_path(@category, @product)
+  	else
+  		render :edit
+  	end
+  end
+
   def show
   end
+
+  def destroy
+  	@product.destroy
+  	redirect_to category_products_path(@category)
+  end
+
+  private
+  	def product_params
+  		params.require(:product).permit(:name, :price, 
+  									:quantity, :description, :brand)
+  	end
+
+  	def set_category
+  		@category = Category.find(params[:category_id])
+  	end
+
+  	def set_product
+  		@product = @category.products.find(params[:id])
+  	end
 end
