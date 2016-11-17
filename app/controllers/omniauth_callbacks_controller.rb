8223b27ea2d8d3ac99d24ac09e8615ca2ac01ec9
Robert Bell <robell7837@hotmail.com>
2016-11-16 17:00:06 -0700

    working github login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -12,6 +12,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    binding.pry
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,7 +7,7 @@ Devise.setup do |config|
   config.omniauth :github,
   ENV["GITHUB_KEY"],
   ENV["GITHUB_SECRET"],
-  scope: "email", info_fields: "email, name"
+  scope: "email"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
